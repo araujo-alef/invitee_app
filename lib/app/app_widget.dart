5d22a3219b82446f40f30bf5ea909a97bae5ee62
araujo-alef <alefaraujo.dev@gmail.com>
2021-10-03 16:08:52 -0400

    update apk_release

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Invitee',
       theme: ThemeData.light(),
